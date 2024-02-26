cbe0c061a1b63da2ce7f7847834d758d285008b7
mh.termzee <m.termzee@basecom.de>
2024-02-26 14:09:24 +0100

    add dummy structures

---
+++ pages/products/[id].vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
+		<ProductDetails :data="product" />
+	</div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from "~/types/Product";
+
+const { id } = useRoute().params;
+const product = ref<Product>();
+const error = useError();
+
+const fetchProduct = async () => {
+	const res = await fetch(`https://fakestoreapi.com/products/${id}`);
+
+	try {
+		const data = await res.json();
+		product.value = data;
+	} catch (err: any) {
+		error.value = err;
+	}
+};
+
+onMounted(() => {
+	fetchProduct();
+});
+</script>
+
+<style scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
+		<h1>Products page</h1>
+		<div class="grid grid-cols-3 gap-4">
+			<div v-for="product in products" :key="product.id">
+				<ProductCard :data="product" />
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from "~/types/Product";
+
+const products = ref<Product[]>([]);
+
+const fetchProducts = async () => {
+	const res = await fetch("https://fakestoreapi.com/products");
+	const data = await res.json();
+	products.value = data;
+};
+
+onMounted(() => {
+	fetchProducts();
+});
+</script>
+
+<style scoped></style>
