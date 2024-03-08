58e12bd91cf757eab98896b276feede9efa5ce58
mh.termzee <m.termzee@basecom.de>
2024-03-08 14:05:41 +0100

    fix design

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,12 +1,14 @@
 <template>
-    <div class="flex flex-col w-4/5 m-auto justify-center items-start">
-        <h1>About page</h1>
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-            perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
-            Repudiandae.
-        </p>
-        <NuxtLink to="/products">Products</NuxtLink>
+    <div class="w-4/5 mx-auto flex justify-center">
+        <div class="flex flex-col justify-center items-start">
+            <h1>About page</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+                perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
+                Repudiandae.
+            </p>
+            <NuxtLink to="/products">Products</NuxtLink>
+        </div>
     </div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,22 @@
 <template>
-	<div class="flex flex-col w-4/5 m-auto justify-center items-start">
-		<h1>Index page</h1>
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
-			perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
-			Repudiandae.
-		</p>
-		<div>
-			<p>Name: {{ name }}</p>
-			<p>Count: {{ store.count }}</p>
-			<p>Double Count: {{ doubleCount }}</p>
-			<button @click="increment">Increment</button>
-			<button @click="decrement">Decrement</button>
-			<button @click="reset">Reset</button>
+	<div class="w-4/5 mx-auto flex justify-center">
+		<div class="flex flex-col justify-center items-start">
+			<h1>Index page</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+				perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
+				Repudiandae.
+			</p>
+			<div>
+				<p>Name: {{ name }}</p>
+				<p>Count: {{ store.count }}</p>
+				<p>Double Count: {{ doubleCount }}</p>
+				<button @click="increment">Increment</button>
+				<button @click="decrement">Decrement</button>
+				<button @click="reset">Reset</button>
+			</div>
+			<NuxtLink to="/about">About</NuxtLink>
 		</div>
-		<NuxtLink to="/about">About</NuxtLink>
 	</div>
 </template>
 
