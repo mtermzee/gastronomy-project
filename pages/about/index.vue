aa6c1c6cf41b7399b5da457e2196b59846a56a84
mh.termzee <m.termzee@basecom.de>
2024-03-08 13:55:14 +0100

    fix about route

---
+++ pages/about/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="flex flex-col w-4/5 m-auto justify-center items-start">
+        <h1>About page</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            perferendis ipsa quae non incidunt, officia dolor ducimus hic corporis.
+            Repudiandae.
+        </p>
+        <NuxtLink to="/products">Products</NuxtLink>
+    </div>
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+    middleware: ['auth']
+})
+</script>
+
+<style scoped></style>
