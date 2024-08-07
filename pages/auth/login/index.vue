e8bbb98a72d5d7611c8244c1b59ae1e3077ed774
mh.termzee <m.termzee@basecom.de>
2024-08-07 14:29:50 +0200

    change btns color

--- pages/auth/login/index.vue
+++ pages/auth/login/index.vue
@@ -29,25 +29,25 @@
                                     Address</label>
                                 <input type="email" name="email" id="email" v-model="email"
                                     placeholder="example@example.com"
-                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-lime-400 focus:ring-lime-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             <div class="mt-6">
                                 <div class="flex justify-between mb-2">
                                     <label for="password" class="text-sm text-gray-300">Password</label>
                                     <a href="#"
-                                        class="text-sm text-gray-400 focus:text-blue-500 hover:text-blue-500 hover:underline">Forgot
+                                        class="text-sm text-gray-400 focus:text-lime-500 hover:text-lime-500 hover:underline">Forgot
                                         password?</a>
                                 </div>
 
                                 <input type="password" name="password" id="password" v-model="password"
                                     placeholder="Your Password"
-                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" />
+                                    class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md focus:border-lime-400 focus:ring-lime-400 focus:outline-none focus:ring focus:ring-opacity-40" />
                             </div>
 
                             <div class="mt-6">
                                 <button
-                                    class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-blue-500 rounded-md hover:bg-blue-400 focus:outline-none focus:bg-blue-400 focus:ring focus:ring-blue-300 focus:ring-opacity-50">
+                                    class="w-full px-4 py-2 tracking-wide text-white transition-colors duration-200 transform bg-lime-500 rounded-md hover:bg-lime-400 focus:outline-none focus:bg-lime-400 focus:ring focus:ring-lime-300 focus:ring-opacity-50">
                                     Sign in
                                 </button>
                             </div>
@@ -55,7 +55,7 @@
 
                         <p class="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <NuxtLink
                                 to="/auth/register"
-                                class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up
+                                class="text-lime-500 focus:outline-none focus:underline hover:underline">Sign up
                             </NuxtLink>.
                         </p>
                     </div>
