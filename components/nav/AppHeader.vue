ed8268d9a95b54347e1294a7c271d71735bfa862
mh.termzee <m.termzee@basecom.de>
2024-08-07 13:14:24 +0200

    fix responsive nav

--- components/nav/AppHeader.vue
+++ components/nav/AppHeader.vue
@@ -1,6 +1,6 @@
 <template>
 	<header class="w-full mb-10 text-white bg-slate-900 shadow-sm body-font">
-		<div class="container flex flex-col items-start justify-between p-6 mx-auto md:flex-row">
+		<div class="container flex flex-col items-center justify-between p-6 mx-auto md:flex-row">
 			<a class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
 				<svg class="w-auto h-5 text-white fill-current" viewBox="0 0 202 69" xmlns="http://www.w3.org/2000/svg">
 					<path
@@ -9,7 +9,7 @@
 				</svg>
 			</a>
 			<nav v-if="user"
-				class="flex flex-wrap items-center justify-center pl-24 text-base md:ml-auto md:mr-auto gap-5">
+				class="flex flex-wrap items-center justify-center pl-0 md:pl-24 text-base md:ml-auto md:mr-auto gap-5">
 				<NuxtLink to="/" class="font-medium hover:text-gray-400">
 					Home
 				</NuxtLink>
@@ -17,15 +17,14 @@
 					Smoothies
 				</NuxtLink>
 			</nav>
-			<div class="items-center h-full" v-if="!user">
-				<NuxtLink to="/auth/login" class="mr-5 font-medium hover:text-gray-400">Login</NuxtLink>
-				<NuxtLink to="/auth/register" class="mr-5 font-medium hover:text-gray-400">
+			<div v-if="!user" class="flex flex-wrap justify-center gap-2 mt-4 md:mt-0">
+				<NuxtLink to="/auth/login" class="mr-0 md:mr-5 font-medium hover:text-gray-400">Login</NuxtLink>
+				<NuxtLink to="/auth/register" class="font-medium hover:text-gray-400">
 					Sign Up
 				</NuxtLink>
 			</div>
-
-			<div class="items-center h-full" v-if="user">
-				<NuxtLink to="/user/profile" class="mr-5 font-medium hover:text-gray-400">Profile</NuxtLink>
+			<div v-if="user" class="flex flex-wrap justify-center items-center gap-2 mt-4 md:mt-0">
+				<NuxtLink to="/user/profile" class="mr-0 md:mr-5 font-medium hover:text-gray-400">Profile</NuxtLink>
 				<button @click="logout"
 					class="px-4 py-2 text-xs font-bold text-white uppercase transition-all duration-150 bg-rose-500 rounded shadow outline-none active:bg-rose-600 hover:shadow-md focus:outline-none ease">Logout</button>
 			</div>
